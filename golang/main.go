3851ca0054eb47e20301680bd43527ee7fd0de5a
dnsmw <danisimeow@gmail.com>
2024-12-25 14:04:13 +0300

    вернул 4 и 6 лабы

--- golang/main.go
+++ golang/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 
-	lab7 "isuct.ru/informatics2022/labs/lab7"
+	"isuct.ru/informatics2022/labs/lab4"
+	"isuct.ru/informatics2022/labs/lab6"
+	"isuct.ru/informatics2022/labs/lab7"
 )
 
 func main() {
 	fmt.Println("Сергеев Данила Владимирович")
+
+	lab4.RunLab4()
+	lab6.Startlab6()
 	lab7.Lab7()
 }
